fbc0835304e3794cdde634375087a034b80e621b
Melissa Melaugh <solarlunix@gmail.com>
2017-10-04 20:17:27 +0000

    Update

---
+++ git.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Pull from main branch
+git pull
+
+# Ensure everything is added and updated
+git add README.md
+git add git.sh
+git add EnergonInc
+
+# Commit changes and upload to the master branch
+git commit -m "Update" 
+git push -u origin master
+
+# Clear the console
+clear
